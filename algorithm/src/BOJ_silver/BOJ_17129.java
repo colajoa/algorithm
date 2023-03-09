7157f369c67b77996210ba0550447ee4e0a2a4f8
ktf1406 <ktf1406@naver.com>
2023-03-09 23:37:19 +0900

    boj

---
+++ algorithm/src/BOJ_silver/BOJ_17129.java
@@ -0,0 +1,14 @@
+package BOJ_silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_17129 {
+    static int 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = null;
+
+    }
+}
