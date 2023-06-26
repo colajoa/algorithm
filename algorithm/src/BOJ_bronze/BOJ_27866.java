84c00531bb3ae001fa909d4e354def5b115f3bd1
ktf1406 <ktf1406@naver.com>
2023-06-26 22:30:12 +0900

    백준 문자와 문자열

---
+++ algorithm/src/BOJ_bronze/BOJ_27866.java
@@ -0,0 +1,11 @@
+package BOJ_bronze;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_27866 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().charAt(Integer.parseInt(br.readLine()) - 1));
+    }
+}
