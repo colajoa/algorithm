532e99d166aad61d5db912ad03b452d07901d6ff
ktf1406 <ktf1406@naver.com>
2023-07-07 20:53:24 +0900

    백준 단어 길이 재기

---
+++ algorithm/src/BOJ_bronze/BOJ_2743.java
@@ -0,0 +1,11 @@
+package BOJ_bronze;
+
+import java.io.*;
+
+public class BOJ_2743 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine().length());
+    }
+}
