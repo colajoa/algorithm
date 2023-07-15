4cd37633d3e6d1f4ab89dfa70085811d4ac6111c
ktf1406 <ktf1406@naver.com>
2023-07-15 22:09:14 +0900

    백준 수학은 체육과목 입니다

---
+++ algorithm/src/BOJ_bronze/BOJ_15984.java
@@ -0,0 +1,10 @@
+package BOJ_bronze;
+
+import java.io.*;
+
+public class BOJ_15984 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Long.parseLong(br.readLine()) * 4);
+    }
+}
