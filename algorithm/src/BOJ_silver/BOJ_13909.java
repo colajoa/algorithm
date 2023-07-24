d686acd65fafe7e4aa54a98f4673511b9d8e4079
ktf1406 <ktf1406@naver.com>
2023-07-24 21:38:26 +0900

    백준 창문 닫기

---
+++ algorithm/src/BOJ_silver/BOJ_13909.java
@@ -0,0 +1,11 @@
+package BOJ_silver;
+
+import java.io.*;
+
+public class BOJ_13909 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println((int) Math.sqrt(N));
+    }
+}
