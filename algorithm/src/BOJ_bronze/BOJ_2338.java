1f9d6c8177bbf157aea6f6e83198f507aa35d14c
ktf1406 <ktf1406@naver.com>
2023-08-01 23:09:32 +0900

    백준 긴자리 계산

---
+++ algorithm/src/BOJ_bronze/BOJ_2338.java
@@ -0,0 +1,18 @@
+package BOJ_bronze;
+
+import java.io.*;
+import java.math.BigInteger;
+
+// 무한대 BigInteger 사용
+public class BOJ_2338 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BigInteger A = new BigInteger(br.readLine());
+        BigInteger B = new BigInteger(br.readLine());
+
+        System.out.println(A.add(B));
+        System.out.println(A.subtract(B));
+        System.out.println(A.multiply(B));
+    }
+}
