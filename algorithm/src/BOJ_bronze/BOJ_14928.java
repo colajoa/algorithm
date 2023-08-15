52f78bb5c825876a86fa48464bf0d059a70c47f0
ktf1406 <ktf1406@naver.com>
2023-08-15 22:56:57 +0900

    백준 큰 수 (BIG)

---
+++ algorithm/src/BOJ_bronze/BOJ_14928.java
@@ -0,0 +1,18 @@
+package BOJ_bronze;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class BOJ_14928 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        long temp = 0;
+        for (int i = 0; i < str.length(); i++) {
+            temp = (temp * 10 + (str.charAt(i) - '0')) % 20000303;
+        }
+
+        System.out.println(temp);
+    }
+}
