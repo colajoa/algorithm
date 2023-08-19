6b2c39a65591f7e4ee619918fd09a3e0f947c1da
ktf1406 <ktf1406@naver.com>
2023-08-19 18:51:52 +0900

    백준 Hook

---
+++ algorithm/src/BOJ_bronze/BOJ_10189.java
@@ -0,0 +1,10 @@
+package BOJ_bronze;
+
+public class BOJ_10189 {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #\n" + //
+                "#### #  # #  # # #\n" + //
+                "#### #  # #  # # #\n" + //
+                "#  # #### #### #  #");
+    }
+}
