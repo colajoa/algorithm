9ef20b0cccac3e7d98ff02973cb50e1dc07b2ee2
ktf1406 <ktf1406@naver.com>
2023-09-21 22:31:12 +0900

    백준 연세대학교

---
+++ algorithm/src/BOJ_bronze/BOJ_15680.java
@@ -0,0 +1,10 @@
+package BOJ_bronze;
+
+import java.io.*;
+
+public class BOJ_15680 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().equals("0") ? "YONSEI" : "Leading the Way to the Future");
+    }
+}
