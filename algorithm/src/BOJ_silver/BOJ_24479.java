57184aea6a10adec02e404980381750b4e08a75c
ktf1406 <ktf1406@naver.com>
2023-10-10 22:45:56 +0900

    백준 알고리즘 수업 - 깊이 우선 탐색 1

---
+++ algorithm/src/BOJ_silver/BOJ_24479.java
@@ -0,0 +1,56 @@
+package BOJ_silver;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_24479 {
+    static int N, M, R, cnt = 1;
+    static int[] visited;
+    static List<Integer>[] list;
+
+    static void dfs(int now) {
+        visited[now] = cnt++;
+
+        for (int next : list[now]) {
+            if (visited[next] == 0) {
+                dfs(next);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(token.nextToken());
+        M = Integer.parseInt(token.nextToken());
+        R = Integer.parseInt(token.nextToken());
+
+        list = new ArrayList[N + 1];
+        visited = new int[N + 1];
+
+        for (int n = 1; n < N + 1; n++) {
+            list[n] = new ArrayList<>();
+        }
+
+        for (int m = 0; m < M; m++) {
+            token = new StringTokenizer(br.readLine());
+
+            int to = Integer.parseInt(token.nextToken());
+            int from = Integer.parseInt(token.nextToken());
+
+            list[to].add(from);
+            list[from].add(to);
+        }
+
+        for (int n = 1; n < N + 1; n++) {
+            Collections.sort(list[n]);
+        }
+
+        dfs(R);
+
+        for (int n = 1; n < N + 1; n++) {
+            System.out.println(visited[n]);
+        }
+    }
+}
