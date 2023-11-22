a927ef5f175eae5ec6cc785e8ad9734d076cfd89
ktf1406 <ktf1406@naver.com>
2023-11-22 18:50:06 +0900

    백준 CAPS

---
+++ algorithm/src/BOJ_bronze/BOJ_15000.java
@@ -0,0 +1,13 @@
+package BOJ_bronze;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_15000 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        System.out.println(str.toUpperCase());
+    }
+}
