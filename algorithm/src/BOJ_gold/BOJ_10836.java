5905dff6e51732614f4370580ea4d84fba8f7ddb
ktf1406 <ktf1406@naver.com>
2024-02-09 18:10:33 +0900

    백준 여왕벌

--- algorithm/src/BOJ_gold/BOJ_10836.java
+++ algorithm/src/BOJ_gold/BOJ_10836.java
@@ -4,49 +4,40 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ_10836 {
-    static int M, N;
-    static int[][] map;
-    static int[] up;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer token = null;
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        token = new StringTokenizer(br.readLine(), " ");
-        M = Integer.parseInt(token.nextToken());
-        N = Integer.parseInt(token.nextToken());
+        int M = Integer.parseInt(token.nextToken());
+        int N = Integer.parseInt(token.nextToken());
 
-        map = new int[M][M];
-        up = new int[(2 * M) - 1];
-        for (int r = 0; r < M; r++) {
-            for (int c = 0; c < M; c++) {
-                map[r][c] = 1;
-            }
-        }
+        int size = 2 * M - 1;
+        int[] side = new int[size];
 
         for (int n = 0; n < N; n++) {
             token = new StringTokenizer(br.readLine(), " ");
-            int idx = 0;
-            int size = 0;
-            while (token.hasMoreTokens()) {
-                int I = Integer.parseInt(token.nextToken());
-                for (int i = 0; i < I; i++) {
-                    up[idx++] = size;
-                }
-                size += 1;
-            }
-            idx = 0;
-            for (int r = M - 1; r > -1; r--) {
-                map[r][0] += up[idx++];
+
+            int zero = Integer.parseInt(token.nextToken());
+            int one = Integer.parseInt(token.nextToken());
+            int two = Integer.parseInt(token.nextToken());
+
+            for (int i = zero; i < zero + one; i++) {
+                side[i] += 1;
             }
 
-            for (int c = 1; c < M; c++) {
-                map[0][c] += up[idx++];
+            for (int i = zero + one; i < size; i++) {
+                side[i] += 2;
             }
         }
 
-        for (int r = 0; r < M; r++) {
-            System.out.println(Arrays.toString(map[r]));
+        for (int i = M - 1; i >= 0; i--) {
+            sb.append(side[i] + 1).append(" ");
+            for (int j = M; j < size; j++) {
+                sb.append(side[j] + 1).append(" ");
+            }
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 }
